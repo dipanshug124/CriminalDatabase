abe6817c0ff41b8a76fb9743d0e42740fa0f1621
Abhi09jeet2000 <65527851+Abhi09jeet2000@users.noreply.github.com>
2020-11-25 20:52:32 +0530

    Add files via upload

--- DBMS_Test/app.py
+++ DBMS_Test/app.py
@@ -2,7 +2,7 @@ from os import abort
 from flask import Flask, render_template, abort
 from flask import session, redirect, url_for,request
 from flask_sqlalchemy import SQLAlchemy
-from forms import EditCriminalRecord,AddCriminal
+from forms import EditCriminalRecord,AddCriminal,Login,AddAdmin
 import sqlite3
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'dfewfew123213rwdsgert34tgfd1234trgf'
@@ -13,20 +13,43 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///criminals.db'
 
 db = SQLAlchemy(app)
 
+"""Is user Logged in"""
+isLogin=False
+
 """Model for criminal."""
 class Criminal(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    photo = db.Column(db.String, unique=True) #name of img
-    fingerprint=db.Column(db.String, unique=True) #name of fingerprintImg
-    Name = db.Column(db.String, unique=True)
-    gender = db.Column(db.String)
-    crime = db.Column(db.String)
-    inJail = db.Column(db.String)
-    country=db.Column(db.String)
+    class Criminal(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        caseno = db.Column(db.String, unique=True)
+        photo = db.Column(db.String, unique=True)  # name of img
+        fingerprint = db.Column(db.String, unique=True)  # name of fingerprintImg
+        Name = db.Column(db.String, unique=True)
+        address = db.Column(db.String)
+        gender = db.Column(db.String)
+        crimelocation = db.Column(db.String)
+        prison = db.Column(db.String)
+        courtname = db.Column(db.String)
+        dutypolice = db.Column(db.String)
+        punishment = db.Column(db.String)
+        crime = db.Column(db.String)
+        startdate = db.Column(db.Date)
+        enddate = db.Column(db.Date)
+
+"""Model for Admin"""
+class Admins(db.Model):
+    id=db.Column(db.Integer,primary_key=True)
+    admin = db.Column(db.String,unique=True)
+    #Name = db.Column(db.String);
+    password=db.Column(db.String,unique=True)
 
 db.create_all()
-
-
+superadmin="Police1234"
+superpassword="1234"
+currentadmin=""
+# Police1234 is superadmin
+# adminUser = Admins(admin="Police1234",password="1234")
+# db.session.add(adminUser)
+# db.session.commit()
 # create all criminals
 
 # domanic = Criminal(photo="1p", Name="Domanic", gender="Male", crime="Hacking", inJail ="Yes", country="Germany" )
@@ -55,27 +78,113 @@ db.create_all()
 def homepage():
     # """View function for home page"""
     criminals = Criminal.query.all()
-    return render_template("home.html", criminals = criminals)
+    type=""
+    if isLogin==False:
+        type="Login"
+    else:
+        type="Logout"
+    return render_template("home.html", criminals = criminals,type=type)
 
 #about
 @app.route("/about")
 def aboutpage():
-    return render_template("about.html")
+    type=""
+    if isLogin==False:
+        type="Login"
+    else:
+        type="Logout"
+    return render_template("about.html" ,type=type)
 
 #search
 @app.route('/search')
 def search():
-    s=request.args.get('search')
-    criminals = Criminal.query.filter(Criminal.Name.like('%'+s+'%'))
-    return render_template('home.html',criminals=criminals)
+    s = request.args.get('search')
+    # criminals = Criminal.query.filter(Criminal.Name.like('%' + s + '%'))
+    # criminals += Criminal.query.filter(Criminal.crime.like('%' + s + '%'))
+    # criminals += Criminal.query.filter(Criminal.dutypolice.like('%' + s + '%'))
+    # criminals += Criminal.query.filter(Criminal.courtname.like('%' + s + '%'))
+    # criminals += Criminal.query.filter(Criminal.prison.like('%' + s + '%'))
+    # criminals += Criminal.query.filter(Criminal.gender.like('%' + s + '%'))
+    criminals = Criminal.query.filter(Criminal.caseno.like('%' + s + '%'))
+    # criminals += Criminal.query.filter(Criminal.crimelocation.like('%' + s + '%'))
+    type = ""
+    if isLogin == False:
+        type = "Login"
+    else:
+        type = "Logout"
+    return render_template('home.html',criminals=criminals,type=type)
 
+#login
+@app.route("/login",methods=["POST","GET"])
+def login():
+    global isLogin
+    global currentadmin
+    if isLogin==True:
+        # global isLogin
+        isLogin=False
+        currentadmin=""
+        criminals = Criminal.query.all()
+        return render_template("home.html", criminals=criminals, type="Login")
+    form = Login()
+    if form.validate_on_submit():
+        username_entered = form.admin.data
+        password_entered = form.password.data
+        # check credentials is invalid
+        user_object = Admins.query.filter_by(admin=username_entered).first()
+        if user_object is None:
+            return render_template("login.html",form = form,message="Username or password is incorrect")
+        elif password_entered != user_object.password:
+            return render_template("login.html",form = form,message="Username or password is incorrect")
+        else:
+            print("login sucessfully")
+            # global isLogin
+            isLogin=True
+            currentadmin=username_entered
+            print(currentadmin)
+            criminals = Criminal.query.all()
+
+            return render_template("home.html", criminals=criminals,type="Logout")
+    return render_template("login.html",form = form)
+
+#add admins by super admin
 #add criminal
+@app.route("/newuser",methods=["POST","GET"])
+def newuser():
+    print(currentadmin)
+    if currentadmin==superadmin and isLogin==True:
+        form=AddAdmin()
+        print(currentadmin==superadmin," ", isLogin)
+        if form.validate_on_submit():
+            ad = Admins(admin=form.admin.data, password=form.password.data)
+            db.session.add(ad)
+            try:
+                db.session.commit()
+            except Exception as e:
+                print(e)
+                db.session.rollback()
+                return render_template("newuser.html", form=form, message="username or password already exists", type="Logout")
+            finally:
+                db.session.close()
+            return render_template("newuser.html", message="Criminal Added Successfully", type="Logout")
+
+        return render_template("newuser.html", form=form,message="Enter username and password ", type="Logout")
+    else:
+        return render_template("newuser.html", message="You are not authorized person to add new admin",
+                               type="Logout")
+
 @app.route("/add", methods=["POST", "GET"])
 def addpage():
+    if isLogin==False:
+        #return render_template("login.html", message="You first Should Login")
+        return redirect(url_for('login'))
     form = AddCriminal()
     if form.validate_on_submit():
-        criminals = Criminal(photo=form.img.data.filename, fingerprint=form.fingerprintImg.data.filename,  Name=form.Name.data, gender=form.gender.data, crime=form.crime.data, inJail=form.inJail.data, country=form.country.data)
-        #form.img.data.read() ->will give use raw data blob ;)
+        criminals = Criminal(photo=form.img.data.filename, fingerprint=form.fingerprintImg.data.filename,
+                             Name=form.Name.data, gender=form.gender.data, crime=form.crime.data,
+                             caseno=form.caseno.data, address=form.address.data, crimelocation=form.crimelocation.data,
+                             prison=form.prison.data, courtname=form.courtname.data, dutypolice=form.dutypolice.data,
+                             punishment=form.punishment.data, startdate=form.startdate.data, enddate=form.enddate.data)
+        # form.img.data.read() ->will give use raw data blob ;)
         db.session.add(criminals)
         print(form.img.data.filename)
         # print("File:{}".format(form.img.data.read()))
@@ -84,34 +193,44 @@ def addpage():
         except Exception as e:
             print(e)
             db.session.rollback()
-            return render_template("add.html",form=form, message="Criminal already exists!")
+            return render_template("add.html",form=form, message="Criminal already exists!",type="Logout")
         finally:
             db.session.close()
-        return render_template("add.html",message="Criminal Added Successfully")
-    return render_template("add.html",form = form)
+        return render_template("add.html",message="Criminal Added Successfully",type="Logout")
+    return render_template("add.html",form = form,type="Logout")
 
 @app.route("/details/<int:criminal_id>", methods=["POST", "GET"])
 def criminal_details(criminal_id):
     """View function for Showing Details of Each Pet."""
+    if isLogin==False:
+        return redirect(url_for('login'))
     #criminal = next((criminal for criminal in criminals if criminal["id"] == criminal_id), None)
     criminal = Criminal.query.get(criminal_id)
+
     form = EditCriminalRecord()
     if criminal is None:
         abort(404, description="No Criminal was Found with the given ID")
     if form.validate_on_submit():
-        criminal.Name=form.Name.data
-        if(len(form.img.data.filename)>0):
-          criminal.photo=form.img.data.filename
-        criminal.gender=form.gender.data
-        criminal.crime=form.crime.data
-        criminal.inJail=form.inJail.data
-        criminal.country=form.country.data
+        criminal.Name = form.Name.data
+        if (len(form.img.data.filename) > 0):
+            criminal.photo = form.img.data.filename
+        criminal.gender = form.gender.data
+        criminal.crime = form.crime.data
+        criminal.caseno = form.caseno.data
+        criminal.address = form.address.data
+        criminal.crimelocation = form.crimelocation.data
+        criminal.prison = form.prison.data
+        criminal.courtname = form.courtname.data
+        criminal.dutypolice = form.dutypolice.data
+        criminal.punishment = form.punishment.data
+        criminal.startdate = form.startdate.data
+        criminal.enddate = form.enddate.data
         try:
             db.session.commit()
         except Exception as e:
             db.session.rollback()
-            return render_template("details.html",criminal=criminal, form=form, message="A Criminal with name already exists!" )
-    return render_template("details.html", criminal=criminal, form = form)
+            return render_template("details.html",criminal=criminal, form=form, message="A Criminal with name already exists!" ,type="Logout")
+    return render_template("details.html", criminal=criminal, form = form, type="Logout")
 
 @app.route("/delete/<int:criminal_id>")
 def delete_criminal(criminal_id):
--- DBMS_Test/forms.py
+++ DBMS_Test/forms.py
@@ -1,23 +1,47 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,FileField, SubmitField
+from wtforms import StringField,FileField, SubmitField, PasswordField,DateField
 from wtforms.validators import InputRequired
 
 class EditCriminalRecord(FlaskForm):
-    Name = StringField("Criminal's Name", validators = [InputRequired()])
-    gender = StringField("Criminal's Gender", validators = [InputRequired()])
+    Name = StringField("Criminal's Full Name", validators=[InputRequired()])
+    caseno = StringField("Case No", validators=[InputRequired()])
     img = FileField("Criminal's Img")
-    crime = StringField("Criminal's crime", validators = [InputRequired()])
-    inJail = StringField("Criminal's status", validators = [InputRequired()])
-    country = StringField("Criminal's country", validators = [InputRequired()])
+    gender = StringField("Criminal's Gender", validators=[InputRequired()])
+    address = StringField("Criminal's Address", validators=[InputRequired()])
+    crime = StringField("Criminal's crime", validators=[InputRequired()])
+    crimelocation = StringField("Crime Location", validators=[InputRequired()])
+    prison = StringField("Prison Location", validators=[InputRequired()])
+    courtname = StringField("Court Name", validators=[InputRequired()])
+    dutypolice = StringField("Duty Police Name", validators=[InputRequired()])
+    punishment = StringField("Punishment", validators=[InputRequired()])
+    startdate = DateField("Start date of punishment", validators=[InputRequired()])
+    enddate = DateField("End date of punishment", validators=[InputRequired()])
     submit = SubmitField("Edit Criminal")
 
 class AddCriminal(FlaskForm):
-    Name = StringField("Criminal's Name", validators=[InputRequired()])
+    Name = StringField("Criminal's Full Name", validators=[InputRequired()])
     gender = StringField("Criminal's Gender", validators=[InputRequired()])
     # photo = StringField("Criminal's Photo Name",validators=[InputRequired()])
-    img = FileField("Criminal's Img",validators=[InputRequired()])
-    fingerprintImg = FileField("Criminal's Fingerprint",validators=[InputRequired()])
+    img = FileField("Criminal's Img", validators=[InputRequired()])
+    fingerprintImg = FileField("Criminal's Fingerprint", validators=[InputRequired()])
     crime = StringField("Criminal's crime", validators=[InputRequired()])
-    inJail = StringField("Criminal's status", validators=[InputRequired()])
-    country = StringField("Criminal's country", validators=[InputRequired()])
+    caseno = StringField("Criminal's Case_No", validators=[InputRequired()])
+    address = StringField("Criminal's Address", validators=[InputRequired()])
+    crimelocation = StringField("Crime Location", validators=[InputRequired()])
+    prison = StringField("Prison Location", validators=[InputRequired()])
+    courtname = StringField("Court Name", validators=[InputRequired()])
+    dutypolice = StringField("Duty Police", validators=[InputRequired()])
+    punishment = StringField("Punishment", validators=[InputRequired()])
+    startdate = DateField("Start date of punishment", validators=[InputRequired()])
+    enddate = DateField("End date of punishment", validators=[InputRequired()])
     submit = SubmitField("Add Criminal")
+
+class Login(FlaskForm):
+    admin = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password",validators=[InputRequired()])
+    submit = SubmitField("Login")
+
+class AddAdmin(FlaskForm):
+    admin = StringField("Username", validators=[InputRequired()])
+    password = PasswordField("Password",validators=[InputRequired()])
+    submit = SubmitField("Add Admin")
